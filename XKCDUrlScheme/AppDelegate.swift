1a3d19e3d976ab7e3f0eb28ecc30011b2d98998f
Antonio Malara <antonio.malara@rooomy.com>
2017-05-10 12:36:44 +0200

    Initial Commit

---
+++ XKCDUrlScheme/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  XKCDUrlScheme
+//
+//  Created by Antonio Malara on 5/10/17.
+//  Copyright © 2017 Antonio Malara. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
